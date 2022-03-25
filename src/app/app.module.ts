e2dba7f9a89d3de93caa2d91a4a88986ab09b95e
Brejcha13320 <davidmejia-2000@outlook.com>
2022-03-24 22:45:32 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
+
+//FORMSMODULE ES PARA TRABAJAR FORMULARIOS POR TEMPLATE
+//REACTIVEFORMSMODULE ES PARA TRABAJAR FORMULARIOS POR DATA
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    DataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
